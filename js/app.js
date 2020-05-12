27c85dd423164b5600a32cd047ffcc53aa0ae6d1
yazan <yazan.alshekha@outlook.com>
2020-05-12 15:32:35 +0300

    init class03

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
 
-
+var counter = 0;
 
 var userName = prompt('enter your name?');
 alert('Wellcome ' + userName);
@@ -12,36 +12,40 @@ var q3 = prompt('does yazan still student?');
 var q4 = prompt('Is yazan studying in cis department? ');
 var q5 = prompt('does yazan live in syria? ');
 
-alert(userName +' you can see results in the console ');
+alert(userName + ' you can see results in the console ');
 
 
 q1 = q1.toLowerCase();
-if (q1 == 'true' || q1 == 't') {
+if (q1 == 'yes' || q1 == 'y') {
     console.log('question 1 is false');
 }
 else {
+    counter++;
     console.log('question 1 is true ');
 }
 
 q2 = q2.toLowerCase();
-if (q2 == 'true' || q2 == 't') {
+if (q2 == 'yes' || q2 == 'y') {
     console.log('question 2 is false');
 }
 else {
+    counter++;
     console.log('question 2 is true ');
 }
 
 q3 = q3.toLowerCase();
-if (q3 == 'true' || q3 == 't') {
+if (q3 == 'yes' || q3 == 'y') {
     console.log('question 3 is false');
 }
 else {
+    counter++;
     console.log('question 3 is true ');
 }
 
 q4 = q4.toLowerCase();
-if (q4 == 'true' || q4 == 't') {
-    console.log('question 4 is true');
+if (q4 == 'yes' || q4 == 'y') {
+    counter++;
+    console.log('question 4 is true ');
 }
 else {
     console.log('question 4 is false ');
@@ -49,11 +53,43 @@ else {
 
 
 q5 = q5.toLowerCase();
-if (q5 == 'true' || q5 == 't') {
+if (q5 == 'yes' || q5 == 'y') {
     console.log('question 5 is false');
 }
 else {
+    counter++;
     console.log('question 5 is true ');
 }
 
 
+
+
+var age = prompt('question six: guess my age ');
+
+for (var i = 0; i < 6; i++) {
+    if (age == 24) { counter++; break; }
+    else if(age>24){ age = prompt('please try again because to hight '); }
+    else if(age>24){ age = prompt('please try again because to low '); }    
+
+}
+
+
+var food=prompt('question seven:guess one of my favorite food  ')
+
+var myFavoritFood=['mansaf','mlo5ea','Grilled chicken breasts']
+
+
+
+var j=0
+for (i=0;i<6;i++){
+    while(j<=myFavoritFood.length){
+        if (food==myFavoritFood[j]){
+            counter++;
+            
+        }
+        break;
+    }
+    break;
+}
+alert(`your score is ${counter} of 7 marks`);
+console.log(`your score is ${counter} of 7 marks`);
